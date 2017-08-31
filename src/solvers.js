8166b04a7422c5e76ac29aed065e8c9936d0aec0
Hack Reactor Student <student@hackreactor.com>
2017-08-30 20:17:25 -0700

    Added pseudocode logic for nRooks

--- src/solvers.js
+++ src/solvers.js
@@ -11,13 +11,44 @@
 // take a look at solversSpec.js to see what the tests are expecting
 
 
-// return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
+// return a matrix (an array of arrays) representing a single nxn chessboard, 
+// with n rooks placed such that none of them can attack each other
 
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-
+  var solution = new Board({n:n});
+  var failCounter = 0;
+  var pieceCounter = 0;
+  var locations = [];
+  
+  
+  var insertingPiece = function(i, j) {
+    // place a piece
+    //loop rows
+    for (var x = i; x < n; x++) {
+    //loop columns
+      for (var y = j; y < n; y++) {
+        solution.togglePiece(x, y);
+        if (solution.hasAnyRowConflicts()) {
+          solution.togglePiece(x, y);
+        } else {
+          //worry about x or y too big
+          insertingPiece(x + 1, y);
+        }
+      }
+      //worry about y too big
+    }
+    //worry about x too big
+    // does it pass the test? 
+    // if pass place another piece on board at next location
+    // if doesn't pass, and the piece not at the end
+    // then pop the piece and place at next location
+    // if doesn't pass and piece is at the end, then pop 2 piece
+    //   
+    
+  };
+  
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
